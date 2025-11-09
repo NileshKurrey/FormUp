2c1903e28d7277eb7512f83ef15610c992a5b118
Nilesh Kurrey <nileshkurrey1@gmail.com>
2025-11-09 21:45:53 +0530

    chore(chortroutes): minor change

--- backend/src/app/routes/CohortRoutes.ts
+++ backend/src/app/routes/CohortRoutes.ts
@@ -3,8 +3,8 @@ import { CohortController } from '../controllers/CohortController.js'
 
 const cohortController = new CohortController()
 export const cohortRouter = Router()
-cohortRouter.post('/', cohortController.createCohort)
+cohortRouter.post('/create', cohortController.createCohort)
 cohortRouter.get('/:id', cohortController.getCohortById)
-cohortRouter.put('/:id', cohortController.updateCohort)
+cohortRouter.put('/update/:id', cohortController.updateCohort)
 // cohortRouter.delete("/:id", cohortController.deleteCohort);
-// cohortRouter.get("/", cohortController.listCohorts);
+cohortRouter.get('/list', cohortController.listCohorts)
